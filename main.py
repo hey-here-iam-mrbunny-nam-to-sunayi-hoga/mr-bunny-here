5039090df887ef4e72babe1843a276289018fe94
🐰 HERE ME MR. BUNNY 🐰 <166007593+hey-here-iam-mrbunny-nam-to-sunayi-hoga@users.noreply.github.com>
2024-05-10 15:54:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,33 @@ app = FastAPI()
 approved_users = {}
 
 @app.get("/approval")
+async def approval_user(key: str = Query(None)):
+    if key is None :
+        return {"message": "Hey its me Hussain Afridi.", "status": "rejected"}
+    elif key in approved_users :
+        return {"message": "Welcome to MR. BUNNY World", "status": "succesfull"}
+    elif key not in approved_users :
+        return {"message": "Hey kids you cant bypass me lol.", "status": "rejected"}
+    else:
+      return {"message": "Hey its me Hussain Afridi.", "status": "rejected"}
+
+@app.get("/user")
 async def approval_user(key: str = Query(None), name: str = Query(None)):
     if key is None or name is None:
-        return {"message": "Hey its me Hussain Afridi.", "status": "false"}
-    if key in approved_users and approved_users[key] == name:
-        return {"message": "User already approved.", "status": "true"}
+        return {"message": "Hey its me Hussain Afridi.", "welcome": "false"}
+    elif key in approved_users and approved_users[key] == name:
+        return {"message": "Welcome to MR. BUNNY World", "welcome": "true"}
     elif key in approved_users and approved_users[key] != name:
-        return {"message": "Hey kids you cant bypass me lol.", "status": "false"}
+        return {"message": "Hey kids you cant bypass me lol.", "welcome": "false"}
     else:
-      return {"message": "Hey its me Hussain Afridi.", "status": "false"}
+      return {"message": "Hey its me Hussain Afridi.", "welcome": "false"}
+@app.delete("/user_removed_by_mr_bunnyxd")
+async def remove_user(key: str = Query(None)):
+    if key in approved_users:
+      del approved_users[key]
+      return {"message": "User removed successfully."}
+    raise HTTPException(status_code=404,detail="User not found")
+    
 @app.get("/userapproval_by_mr_bunnyxd")
 async def approve_user(key: str = Query(None), name: str = Query(None)):
     approved_users[key] = name
@@ -25,7 +43,7 @@ async def approve_status(key: str = Query(None), name: str = Query(None)):
         return {"message": "Key and name are required."}
     
     if key in approved_users and approved_users[key] == name:
-        return {"message": "Approved successfully."}
+        return {"message": "User already Approved."}
     else:
         return {"message": "Approval failed."}
 
